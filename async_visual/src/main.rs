0147c5a5e5e4ad6322005dcd781090df8f8ea5ed
dj8yf0μl <sffhox@gmail.com>
2022-08-25 15:22:09 +0300

    async fn/vs fn returning an async block: async_visual

--- async_visual/src/main.rs
+++ async_visual/src/main.rs
@@ -4,7 +4,37 @@ use colored::{Color, Colorize};
 use std::future::Future;
 
 type LocalRes = std::io::Result<(String, Color)>;
+
 fn cheapo_request<'b, 'a: 'b>(
+    host: &'a str,
+    port: u16,
+    path: &'b str,
+    color: Color,
+) -> impl Future<Output = LocalRes> + 'static {
+    let host = host.to_string();
+    let path = path.to_string();
+    async move {
+        eprintln!("{}", "Awaiting connection retrival...".color(color));
+
+        let mut socket = net::TcpStream::connect((host.as_ref(), port)).await?;
+        eprintln!("obtained {}", format!("{:?}", socket).color(color));
+
+        let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\n\r\n", path, host);
+        eprintln!("{}", "sending request...".color(color));
+        socket.write_all(request.as_bytes()).await?;
+        socket.shutdown(net::Shutdown::Write)?;
+        eprintln!("{}", "request sent".color(color));
+
+        let mut response = String::new();
+        eprintln!("{}", "reading response...".color(color));
+        socket.read_to_string(&mut response).await?;
+        eprintln!("{}", "retrived response".color(color));
+
+        Ok((response, color))
+    }
+}
+
+fn cheapo_request1<'b, 'a: 'b>(
     host: &'a str,
     port: u16,
     path: &'b str,
@@ -31,7 +61,7 @@ fn cheapo_request<'b, 'a: 'b>(
     }
 }
 
-async fn many_requests(
+async fn many_requests1(
     requests: Vec<(String, u16, String, Color)>,
 ) -> Vec<std::io::Result<(String, Color)>> {
     use async_std::task;
@@ -39,9 +69,27 @@ async fn many_requests(
     let mut handles = vec![];
     for (host, port, path, color) in requests {
         handles.push(task::spawn_local(async move {
-            let res = cheapo_request(&host, port, &path, color).await?;
+            let res = cheapo_request1(&host, port, &path, color).await?;
             Ok(res)
         }));
+    }
+
+    let mut results = vec![];
+    for handle in handles {
+        results.push(handle.await);
+    }
+
+    results
+}
+
+async fn many_requests(
+    requests: Vec<(String, u16, String, Color)>,
+) -> Vec<std::io::Result<(String, Color)>> {
+    use async_std::task;
+
+    let mut handles = vec![];
+    for (host, port, path, color) in requests {
+        handles.push(task::spawn_local(cheapo_request(&host, port, &path, color)));
     }
 
     let mut results = vec![];
